41b2693c1dc4ddd35136f4df0e80b94d9774a2fd
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2024-08-06 14:59:00 +0530

    Time: 31 ms (83.65%) | Memory: 16.5 MB (23.74%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        s={}
+        for i in word:
+            if i not in s:
+                s[i]=1
+            else:
+                s[i]+=1
+        s= {key: value for key, value in sorted(s.items(), key=lambda item: item[1], reverse=True)}
+        sum=0
+        k=0
+        for i in s:
+           sum+=s[i]*(k//8 +1)
+           k+=1
+          
+        return sum
