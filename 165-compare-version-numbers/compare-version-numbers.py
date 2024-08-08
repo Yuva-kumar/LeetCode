d2861fde15a4a54cc5042bb6cd99b4fa56f007ab
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2024-08-08 15:11:56 +0530

    Time: 32 ms (74.42%) | Memory: 16.6 MB (5.43%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,21 +1,7 @@
 class Solution:
     def compareVersion(self, v1: str, v2: str) -> int:
-        def fun(v):
-            l=[]
-            a=''
-            for i in v:
-                if i!='.':
-                    a+=i
-                else:
-                    if len(a)==0:
-                        l.append(0)
-                    else:
-                        l.append(int(a))
-                    a=""
-            l.append(int(a))
-            return l
-        a=fun(v1)
-        b=fun(v2)
+        a=v1.split('.')
+        b=v2.split('.')
         if len(a)!=len(b):
             k=min(len(a),len(b))
             if k==len(a):
@@ -23,8 +9,8 @@ class Solution:
             else:
                 b=b+[0]*(len(a)-len(b))
         for i in range(len(a)):
-            if a[i]>b[i]:
+            if int(a[i])>int(b[i]):
                 return 1
-            elif b[i]>a[i]:
+            elif int(b[i])>int(a[i]):
                 return -1
         return 0
