d8af33eef3d20cd5c64fe332a78b4af2df8da84a
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2024-08-24 21:57:28 +0530

    Time: 7 ms (15.64%) | Memory: 14.2 MB (53.01%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,28 +11,46 @@
  */
 class Solution {
 public:
+void fun(vector<int>&v,int i,TreeNode* root){
+    if (root==NULL){
+        return ;
+    }
+    if (v.size()==i) v.push_back(root->val);
+    fun(v,i+1,root->right);
+    fun(v,i+1,root->left);
+}
     vector<int> rightSideView(TreeNode* root) {
         vector<int>v;
-        if (root==NULL) return v;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int k=q.size();
-            for(int i=0;i<k;i++){
-                TreeNode* temp=q.front();
-                q.pop();
-                if (i==k-1){
-                    v.push_back(temp->val);
-                }
-                if (temp->left!=NULL){
-                    q.push(temp->left);
-                }
-                if (temp->right!=NULL){
-                    q.push(temp->right);
-                }
-            }            
-        }
-        return v;
+        fun(v,0,root);
+        return v;        
+
+
+
+
+
+
+
+        // vector<int>v;
+        // if (root==NULL) return v;
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int k=q.size();
+        //     for(int i=0;i<k;i++){
+        //         TreeNode* temp=q.front();
+        //         q.pop();
+        //         if (i==k-1){
+        //             v.push_back(temp->val);
+        //         }
+        //         if (temp->left!=NULL){
+        //             q.push(temp->left);
+        //         }
+        //         if (temp->right!=NULL){
+        //             q.push(temp->right);
+        //         }
+        //     }            
+        // }
+        // return v;
 
     }  
     
