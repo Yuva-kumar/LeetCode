b7a53e954cb44f14c6cc661c0b171f4903704392
Yuva-kumar <99953484+Yuva-kumar@users.noreply.github.com>
2025-07-25 22:34:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (85.25%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -1,13 +1,15 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
         
-
         if max(nums) < 0:
             return max(nums)
         
-        l = []
+        l = [0]*101
+        res = 0
         for i in nums:
-            if i >= 0:
-                if i not in l:
-                    l.append(i)
-        return sum(l)
+            if (i >= 0) and (l[i] == 0):
+                res += i 
+                l[i] = 1
+        return res
+
+        
